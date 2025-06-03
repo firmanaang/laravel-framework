8c7e7498cd90dbbd4f67d440ab17a493484ea2ea
Prihan Firmanullah <42978169+firmanaang@users.noreply.github.com>
2025-06-03 20:40:47 +0700

    Add resources/js/composables/i18n/permissions.ts 1748958047459

---
+++ resources/js/composables/i18n/permissions.ts
@@ -0,0 +1,81 @@
+export const permissionTranslations = {
+  // Permissions - Units
+  "permission.viewUnits": {
+    en: "You do not have permission to view units",
+    id: "Anda tidak memiliki izin untuk melihat satuan",
+  },
+  "permission.createUnits": {
+    en: "You do not have permission to create units",
+    id: "Anda tidak memiliki izin untuk membuat satuan",
+  },
+  "permission.editUnits": {
+    en: "You do not have permission to edit units",
+    id: "Anda tidak memiliki izin untuk mengedit satuan",
+  },
+  "permission.deleteUnits": {
+    en: "You do not have permission to delete units",
+    id: "Anda tidak memiliki izin untuk menghapus satuan",
+  },
+
+  // Permissions - Categories
+  "permission.viewCategories": {
+    en: "You do not have permission to view categories",
+    id: "Anda tidak memiliki izin untuk melihat kategori",
+  },
+  "permission.createCategories": {
+    en: "You do not have permission to create categories",
+    id: "Anda tidak memiliki izin untuk membuat kategori",
+  },
+  "permission.editCategories": {
+    en: "You do not have permission to edit categories",
+    id: "Anda tidak memiliki izin untuk mengedit kategori",
+  },
+  "permission.deleteCategories": {
+    en: "You do not have permission to delete categories",
+    id: "Anda tidak memiliki izin untuk menghapus kategori",
+  },
+
+  // Permissions - Brands
+  "permission.viewBrands": {
+    en: "You do not have permission to view brands",
+    id: "Anda tidak memiliki izin untuk melihat merek",
+  },
+  "permission.createBrands": {
+    en: "You do not have permission to create brands",
+    id: "Anda tidak memiliki izin untuk membuat merek",
+  },
+  "permission.editBrands": {
+    en: "You do not have permission to edit brands",
+    id: "Anda tidak memiliki izin untuk mengedit merek",
+  },
+  "permission.deleteBrands": {
+    en: "You do not have permission to delete brands",
+    id: "Anda tidak memiliki izin untuk menghapus merek",
+  },
+
+  // General Permissions
+  "permission.viewUsers": {
+    en: "You do not have permission to view users",
+    id: "Anda tidak memiliki izin untuk melihat pengguna",
+  },
+  "permission.createUsers": {
+    en: "You do not have permission to create users",
+    id: "Anda tidak memiliki izin untuk membuat pengguna",
+  },
+  "permission.editUsers": {
+    en: "You do not have permission to edit users",
+    id: "Anda tidak memiliki izin untuk mengedit pengguna",
+  },
+  "permission.deleteUsers": {
+    en: "You do not have permission to delete users",
+    id: "Anda tidak memiliki izin untuk menghapus pengguna",
+  },
+  "permission.viewSettings": {
+    en: "You do not have permission to view settings",
+    id: "Anda tidak memiliki izin untuk melihat pengaturan",
+  },
+  "permission.editSettings": {
+    en: "You do not have permission to edit settings",
+    id: "Anda tidak memiliki izin untuk mengedit pengaturan",
+  },
+}
