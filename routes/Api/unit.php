cb69a2f2c4a2e15e2d53311b7cbac7b24ce0c98e
Prihan Firmanullah <42978169+firmanaang@users.noreply.github.com>
2025-06-03 20:39:04 +0700

    Add routes/Api/unit.php 1748957944549

---
+++ routes/Api/unit.php
@@ -0,0 +1,13 @@
+<?php
+
+use App\Http\Controllers\Api\UnitController;
+use Illuminate\Support\Facades\Route;
+
+Route::middleware('auth:sanctum')->group(function () {
+    // Units API
+    Route::get('units', [UnitController::class, 'index'])->middleware('permission:view units');
+    Route::post('units', [UnitController::class, 'store'])->middleware('permission:create units');
+    Route::put('units/{id}', [UnitController::class, 'update'])->middleware('permission:edit units');
+    Route::delete('units/{id}', [UnitController::class, 'destroy'])->middleware('permission:delete units');
+    Route::post('units/bulk-delete', [UnitController::class, 'bulkDestroy'])->middleware('permission:delete units');
+});
